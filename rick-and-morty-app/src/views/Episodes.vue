8b391a9ff89b9bc396983801708283544515cc7e
Arlan <145493122+Arlan-Z@users.noreply.github.com>
2024-08-06 00:35:59 +0500

    pagination added

--- rick-and-morty-app/src/views/Episodes.vue
+++ rick-and-morty-app/src/views/Episodes.vue
@@ -1,28 +1,57 @@
 <template>
-    <div>
-      <h1>Episodes</h1>
-      <div v-for="episode in episodes" :key="episode.id">
-        <router-link :to="`/episode/${episode.id}`">{{ episode.name }}</router-link>
-        <p>{{ episode.air_date }}</p>
-      </div>
+  <div>
+    <h1>Episodes</h1>
+    <div v-for="episode in episodes" :key="episode.id">
+      <router-link :to="`/episode/${episode.id}`">{{ episode.name }}</router-link>
+      <p>{{ episode.air_date }}</p>
     </div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    data() {
-      return {
-        episodes: [],
-      };
-    },
-    created() {
-      axios.get('https://rickandmortyapi.com/api/episode')
+
+    <div class="pagination">
+      <button @click="fetchEpisodes(currentPage - 1)" :disabled="currentPage === 1">Previous</button>
+      <span>Page {{ currentPage }} of {{ totalPages }}</span>
+      <button @click="fetchEpisodes(currentPage + 1)" :disabled="currentPage === totalPages">Next</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      episodes: [],
+      currentPage: 1,
+      totalPages: 1,
+    };
+  },
+  created() {
+    this.fetchEpisodes(this.currentPage);
+  },
+  methods: {
+    fetchEpisodes(page) {
+      if (page < 1 || page > this.totalPages) return;
+
+      axios.get(`https://rickandmortyapi.com/api/episode?page=${page}`)
         .then(response => {
           this.episodes = response.data.results;
+          this.currentPage = page;
+          this.totalPages = response.data.info.pages;
         });
     },
-  };
-  </script>
-  
+  },
+};
+</script>
+
+<style>
+.pagination {
+  margin-top: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+button {
+  margin: 0 10px;
+}
+</style>
--- rick-and-morty-app/src/views/Locations.vue
+++ rick-and-morty-app/src/views/Locations.vue
@@ -1,28 +1,58 @@
 <template>
-    <div>
-      <h1>Locations</h1>
-      <div v-for="location in locations" :key="location.id">
-        <router-link :to="`/location/${location.id}`">{{ location.name }}</router-link>
-        <p>{{ location.type }}</p>
-      </div>
+  <div>
+    <h1>Locations</h1>
+    <div v-for="location in locations" :key="location.id">
+      <router-link :to="`/location/${location.id}`">{{ location.name }}</router-link>
+      <!-- <p>{{ location.type }}</p> -->
     </div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    data() {
-      return {
-        locations: [],
-      };
-    },
-    created() {
-      axios.get('https://rickandmortyapi.com/api/location')
+
+    <div class="pagination">
+      <button @click="fetchLocations(currentPage - 1)" :disabled="currentPage === 1">Previous</button>
+      <span>Page {{ currentPage }} of {{ totalPages }}</span>
+      <button @click="fetchLocations(currentPage + 1)" :disabled="currentPage === totalPages">Next</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      locations: [],
+      currentPage: 1,
+      totalPages: 1,
+    };
+  },
+  created() {
+    this.fetchLocations(this.currentPage);
+  },
+  methods: {
+    fetchLocations(page) {
+      if (page < 1 || page > this.totalPages) return;
+      
+      axios.get(`https://rickandmortyapi.com/api/location?page=${page}`)
         .then(response => {
           this.locations = response.data.results;
+          this.currentPage = page;
+          this.totalPages = response.data.info.pages;
         });
     },
-  };
-  </script>
-  
+  },
+};
+</script>
+
+<style>
+/* Добавьте стили для улучшения отображения, если необходимо */
+.pagination {
+  margin-top: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+button {
+  margin: 0 10px;
+}
+</style>
