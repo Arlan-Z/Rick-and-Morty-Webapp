86037f54c7be601cf47f37150c459ca4b21bf2ac
Arlan <145493122+Arlan-Z@users.noreply.github.com>
2024-08-06 13:02:58 +0500

    episode details and location detaild changed

--- rick-and-morty-app/src/views/EpisodeDetails.vue
+++ rick-and-morty-app/src/views/EpisodeDetails.vue
@@ -1,33 +1,57 @@
 <template>
-    <div v-if="episode">
-      <h1>{{ episode.name }}</h1>
-      <p>Air Date: {{ episode.air_date }}</p>
-      <p>Episode: {{ episode.episode}}</p>
-      <h2>Characters</h2>
-      <ul>
-        <li v-for="character in episode.characters" :key="character">
-          <router-link :to="`/character/${character.split('/').pop()}`">{{ character.split('/').pop() }}</router-link>
-        </li>
-      </ul>
-    </div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    data() {
-      return {
-        episode: null,
-      };
-    },
-    created() {
-      const id = this.$route.params.id;
-      axios.get(`https://rickandmortyapi.com/api/episode/${id}`)
-        .then(response => {
-          this.episode = response.data;
-        });
-    },
-  };
-  </script>
-  
+  <div v-if="episode">
+    <h1>{{ episode.name }}</h1>
+    <p>Air Date: {{ episode.air_date }}</p>
+    <p>Episode: {{ episode.episode}}</p>
+    <h2>Characters</h2>
+    <ul>
+      <li v-for="character in characters" :key="character.id">
+        <router-link :to="`/character/${character.id}`">{{ character.name }}</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      episode: null,
+      characters: [],
+    };
+  },
+  created() {
+    const id = this.$route.params.id;
+    axios.get(`https://rickandmortyapi.com/api/episode/${id}`)
+      .then(response => {
+        this.episode = response.data;
+        return Promise.all(response.data.characters.map(url => axios.get(url)));
+      })
+      .then(characterResponses => {
+        this.characters = characterResponses.map(response => response.data);
+      });
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  margin-bottom: 10px;
+}
+
+a {
+  text-decoration: none;
+  color: #1a73e8;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+</style>
--- rick-and-morty-app/src/views/LocationDetails.vue
+++ rick-and-morty-app/src/views/LocationDetails.vue
@@ -1,33 +1,57 @@
 <template>
-    <div v-if="location">
-      <h1>{{ location.name }}</h1>
-      <p>Type: {{ location.type }}</p>
-      <p>Dimension: {{ location.dimension }}</p>
-      <h2>Residents</h2>
-      <ul>
-        <li v-for="resident in location.residents" :key="resident">
-          <router-link :to="`/character/${resident.split('/').pop()}`">{{ resident.split('/').pop() }}</router-link>
-        </li>
-      </ul>
-    </div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    data() {
-      return {
-        location: null,
-      };
-    },
-    created() {
-      const id = this.$route.params.id;
-      axios.get(`https://rickandmortyapi.com/api/location/${id}`)
-        .then(response => {
-          this.location = response.data;
-        });
-    },
-  };
-  </script>
-  
+  <div v-if="location">
+    <h1>{{ location.name }}</h1>
+    <p>Type: {{ location.type }}</p>
+    <p>Dimension: {{ location.dimension }}</p>
+    <h2>Residents</h2>
+    <ul>
+      <li v-for="resident in residents" :key="resident.id">
+        <router-link :to="`/character/${resident.id}`">{{ resident.name }}</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      location: null,
+      residents: [],
+    };
+  },
+  created() {
+    const id = this.$route.params.id;
+    axios.get(`https://rickandmortyapi.com/api/location/${id}`)
+      .then(response => {
+        this.location = response.data;
+        return Promise.all(response.data.residents.map(url => axios.get(url)));
+      })
+      .then(residentResponses => {
+        this.residents = residentResponses.map(response => response.data);
+      });
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  margin-bottom: 10px;
+}
+
+a {
+  text-decoration: none;
+  color: #1a73e8;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+</style>
